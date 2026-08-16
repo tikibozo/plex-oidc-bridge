08a7e66849da793d4e5d9121f96818d110b38a8d
tikibozo <4793440+tikibozo@users.noreply.github.com>
2026-08-16 03:03:18 -0700

    chore(main): release 0.6.9 (#36)

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ import (
 const (
 	plexAPIBaseURL        = "https://plex.tv/api/v2"
 	defaultProductName    = "PlexOIDCBridge"
-	productVersion        = "0.6.8" // x-release-please-version
+	productVersion        = "0.6.9" // x-release-please-version
 	clientID              = "plex-oidc-bridge-docker"
 	maxStateSize          = 4096
 	maxNonceSize          = 4096
